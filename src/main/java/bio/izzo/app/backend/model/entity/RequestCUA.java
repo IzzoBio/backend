17b1693525eea2ed9fb9b34dd5a44c6514c3a701
Daris02 <hei.raymond.2@gmail.com>
2024-08-17 07:51:17 +0300

    fix: update request cua

--- src/main/java/bio/izzo/app/backend/model/entity/RequestCUA.java
+++ src/main/java/bio/izzo/app/backend/model/entity/RequestCUA.java
@@ -2,11 +2,13 @@ package bio.izzo.app.backend.model.entity;
 
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
-@Data
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestCUA extends AbstractEntity {
